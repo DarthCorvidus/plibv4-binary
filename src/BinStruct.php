b79b6867cbebda6c54c80ade28fede23ddf5e042
DarthCorvidus <github@vm01.telton.de>
2023-06-10 19:52:35 +0200

    Interface for a collection of BinVal and BinStruct definitions/objects.

---
+++ src/BinStruct.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Interface for a binary structure, which defines a collection of both BinVals
+ * and BinStructs, to define complex and nested binary files.
+ */
+interface BinStruct {
+	/**
+	 * get names of entries.
+	 */
+	function getNames(): array;
+	/**
+	 * Should return true if an entry is an instance of BinVal
+	 * @param string $name
+	 */
+	function isBinVal(string $name): bool;
+	/**
+	 * Should return true if an entry is an instance of BinStruct
+	 * @param string $name
+	 */
+	function isBinStruct(string $name): bool;
+	/**
+	 * Return entry as BinVal
+	 * @param string $name
+	 */
+	function getBinVal(string $name): BinVal;
+	/**
+	 * Return entry as BinStruct
+	 * @param string $name
+	 */
+	function getBinStruct(string $name): BinStruct;
+}
