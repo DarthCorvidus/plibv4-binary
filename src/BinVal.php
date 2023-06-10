84e037202f1b87a0283c28385faffa00e37825d2
DarthCorvidus <github@vm01.telton.de>
2023-06-10 19:40:56 +0200

    Interface which defines an import class for a single binary value.

---
+++ src/BinVal.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Interface for a single binary value, to be read or written from or to a file
+ * or string.
+ * @copyright (c) 2023 Claus-Christoph Küthe
+ * @author Claus-Christoph Küthe <floss@vm01.telton.de>
+ */
+interface BinVal {
+	/**
+	 * This is supposed to return a binary string ready to be written to a
+	 * binary file.
+	 * @param type $value
+	 */
+	function putValue($value): string;
+	/**
+	 * This is supposed to convert a binary value into a PHP datatype, ie 'a'
+	 * becomes 100 when read as int.
+	 * 
+	 * @param string $string
+	 */
+	function getValue(string $string);
+	/**
+	 * Length of the binary data in bytes, ie a 32bit integer would be 4 bytes
+	 * long.
+	 */
+	function getLength(): int;
+}
