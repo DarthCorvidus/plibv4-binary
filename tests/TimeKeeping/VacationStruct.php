c0d7e5a3f78269554ada3c38ea1212a7ead88852
DarthCorvidus <github@vm01.telton.de>
2025-01-06 13:47:28 +0100

    Psalm's objection does not make that much of a sense here.

--- tests/TimeKeeping/VacationStruct.php
+++ tests/TimeKeeping/VacationStruct.php
@@ -3,6 +3,9 @@ namespace plibv4\binary\tests;
 use plibv4\binary\Structure;
 use plibv4\binary\UnsignedInteger8;
 use plibv4\binary\UnsignedInteger32;
+/**
+ * @psalm-suppress MissingConstructor
+ */
 class VacationStruct implements Structure {
 	public UnsignedInteger32 $from;
 	public UnsignedInteger32 $to;
--- tests/autoload.php
+++ tests/autoload.php
@@ -2,5 +2,8 @@
 require_once __DIR__."/../vendor/autoload.php";
 $dir = dirname(__DIR__)."/tests/TimeKeeping";
 foreach(glob($dir."/*.php") as $value) {
+	/**
+	 * @psalm-suppress UnresolvableInclude
+	 */
 	require_once $value;
 }
