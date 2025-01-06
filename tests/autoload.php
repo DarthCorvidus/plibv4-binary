d9b43e14e39811788b8f6eb87a6b17b8526e1c9a
DarthCorvidus <github@vm01.telton.de>
2025-01-06 13:37:17 +0100

    Well, commenting out the includes you actually need is a good move.

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,7 +1,6 @@
 <?php
-echo __DIR__.PHP_EOL;
 require_once __DIR__."/../vendor/autoload.php";
-#$dir = dirname(__DIR__)."/tests/TimeKeeping";
-#foreach(glob($dir."/*.php") as $value) {
-#	require_once $value;
-#}
+$dir = dirname(__DIR__)."/tests/TimeKeeping";
+foreach(glob($dir."/*.php") as $value) {
+	require_once $value;
+}
