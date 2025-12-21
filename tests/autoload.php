a92d7f19008ca91ab4b225ec354691da228c644c
DarthCorvidus <github@vm01.telton.de>
2025-12-21 14:50:42 +0100

    chore: remove some Psalm findings

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,14 +1,22 @@
 <?php
 require_once __DIR__."/../vendor/autoload.php";
 $dir = dirname(__DIR__)."/tests/TimeKeeping";
-foreach(glob($dir."/*.php") as $value) {
+$glob = glob($dir."/*.php");
+if($glob === false) {
+	throw new \RuntimeException("unable to iterate over ".$dir);
+}
+foreach($glob as $value) {
 	/**
 	 * @psalm-suppress UnresolvableInclude
 	 */
 	require_once $value;
 }
 $examples = dirname(__DIR__)."/tests/StructExamples";
-foreach(glob($examples."/*.php") as $value) {
+$globExamples = glob($examples."/*.php");
+if($globExamples === false) {
+	throw new \RuntimeException("unable to iterate over ".$examples);
+}
+foreach($globExamples as $value) {
 	/**
 	 * @psalm-suppress UnresolvableInclude
 	 */
